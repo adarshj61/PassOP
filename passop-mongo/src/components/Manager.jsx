a3251231c3e5214abb0de239a7eb6d496ce45bd5
adarshj61 <123823474+adarshj61@users.noreply.github.com>
2026-03-09 10:26:14 +0530

    Fix fetch URL to use production endpoint

--- passop-mongo/src/components/Manager.jsx
+++ passop-mongo/src/components/Manager.jsx
@@ -11,7 +11,7 @@ const Manager = () => {
     const [passwordArray, setPasswordArray] = useState([])
 
     const getPasswords = async () => {
-        let req = await fetch("http://localhost:3000/")
+        let req = await fetch("hhttps://pass-op-eta.vercel.app")
         let passwords = await req.json()
         setPasswordArray(passwords)
     }
